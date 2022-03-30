bd0785295db296eeb5683cf2fae672bae3e2e017
udithkav <67887799+udithkav@users.noreply.github.com>
2022-03-30 20:43:09 +0530

    Added user profile page

---
+++ crazy_alarm_app/lib/util/user_preferences.dart
@@ -0,0 +1,5 @@
+import 'package:crazy_alarm_app/model/user.dart';
+
+class UserPreferences{
+  static const myUser = User(phoneNumber: '+94778458794', email: 'janedoe666@gmail.com', imagePath: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60', name: 'Jane Doe', dob: '1997/07/01');
+}
