17869465dc8b501666140200e6c4fbd92fafd6a7
trwelk <weerasooriya.trewon@gmail.com>
2022-04-07 17:04:26 +0530

    Backend of alarm history

--- alarm-backend/server.js
+++ alarm-backend/server.js
@@ -1,6 +1,11 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser')
 const cors = require('@koa/cors');
+const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb+srv://ifsapp:ifsapp@cluster0.cc6tj.mongodb.net/crazy-alarm?retryWrites=true&w=majority');
+
 const corsOptions ={
     origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
@@ -28,6 +33,5 @@ app.use(cors(corsOptions));
  app.use(NotificationRoutes.routes())
  .use(NotificationRoutes.allowedMethods());
 
-app.listen(9090);
-
+app.listen(process.env.PORT || 9090)
 console.log('Application is running on port 9090');
