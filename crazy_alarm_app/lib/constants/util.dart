6f9ccfc1a4851975cb53cfcb39fc8928a7816ea0
IT19163864 <64912486+ryanmlk@users.noreply.github.com>
2022-04-07 20:43:04 +0530

    Fixed some build issues

--- crazy_alarm_app/lib/constants/util.dart
+++ crazy_alarm_app/lib/constants/util.dart
@@ -1,4 +1,4 @@
-const apiUrl = 'http://192.168.1.3:9090';
+const apiUrl = 'https://crazy-alarm-api.herokuapp.com';
 const notificationRoute = '/notifications';
 const alarmRoute = '/alarms';
 const userRoute = '/user';
